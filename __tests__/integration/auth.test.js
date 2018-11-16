727a0d2f19068d0a5d7e1cf651e7c83789a64b6a
lmedson <medson-mbp@hotmail.com>
2018-11-16 14:46:54 -0300

    Add mock and stub to sendmail

--- __tests__/integration/auth.test.js
+++ __tests__/integration/auth.test.js
@@ -1,5 +1,13 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+const nodemailer = require('nodemailer');
+
+const transport = {
+  sendMail: sinon.spy(),
+};
+
+sinon.stub(nodemailer, 'createTransport').returns(transport);
 
 const { expect } = chai;
 
@@ -27,6 +35,7 @@ describe('Authentication', () => {
 
       expect(response.body).to.have.property('user');
       expect(response.body).to.have.property('token');
+      expect(transport.sendMail.calledOnce).to.be.true;
     });
     it('it should not to be able to sign up with duplicates', async () => {
       const user = await factory.create('User');
